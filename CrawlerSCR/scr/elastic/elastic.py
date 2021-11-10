28b0f041498d5f983bf950cdb6bb1e6ead9339ec
David B <40240397+dabm-git@users.noreply.github.com>
2021-11-10 10:54:05 +0100

    elastic _id to full_name

--- CrawlerSCR/scr/elastic/elastic.py
+++ CrawlerSCR/scr/elastic/elastic.py
@@ -59,7 +59,7 @@ class elastic():
             yield {
                     "_index": 'scr_index', # index of crawled data
                     #"_type": "_doc", # decrapated
-                    "_id" : f"{document['uuid']}", # use the full name as id user+serviceName
+                    "_id" : f"{document['full_name']}", # use the full name as id user+serviceName
                     "_source": document.to_dict(), # use a filter?
                 }    
         
