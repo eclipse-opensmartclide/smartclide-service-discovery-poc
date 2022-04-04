d8059bcbe89901a0f936eb09df27f6cd0e12d63f
David B <40240397+dabm-git@users.noreply.github.com>
2022-04-04 15:50:12 +0200

    json input fix

--- ServiceDiscovery/servicediscovery/api/search_api.py
+++ ServiceDiscovery/servicediscovery/api/search_api.py
@@ -1,33 +1,35 @@
 #!flask/bin/python
 # Eclipse Public License 2.0
 
-import json
 from flask import request
 from flask_restx import Resource
 
 from api.v1 import api
 from core import cache, limiter
 from elastic.elasticsearch import Elastic
+from utils import PrintLog, FlaskUtils
 
 search_ns = api.namespace('service_search', description='Search for a service in the registry')
 @search_ns.route('', methods = ['POST']) # url/user
 class GetStatus(Resource):
     @limiter.limit('1000/hour')
+    @cache.cached(timeout=1, query_string=True)
     @api.response(404, 'Data not found')
     @api.response(500, 'Unhandled errors')
     @api.response(400, 'Invalid parameters')
+    @api.response(503, 'Service Unavailable')
     def post(self):
-        if request.method == 'POST':
-            data = request.json
+        if request.method != 'POST':
+            return
 
-            try:
-                json.loads(data)
-            except TypeError:
-                return 'Invalid parameter format, only JSON is acepted', 400
+        if (data := request.get_json()) is None:                
+            FlaskUtils.handle400error(search_ns, "Empty POST data")
 
-            if data is None:
-                return {'message': 'Empty POST content'}, 400
-            
-            # The validation is done in the Elastic class
-            l_elastic = Elastic()
-            return  l_elastic.search(data)
+        try: 
+            # If elasticsearch is not available, dont even try to build the query            
+            srch = Elastic().search(data)
+        except Exception as e:
+            PrintLog.log("[service_search] Search service is unavailable, check the Elasticsearch status")               
+            FlaskUtils.handle503error(search_ns, 'Search service is unavailable')
+    
+        return srch
--- ServiceDiscovery/servicediscovery/api/v1.py
+++ ServiceDiscovery/servicediscovery/api/v1.py
@@ -5,68 +5,81 @@ from flask_restx import Api
 from flask_restx import Resource
 from flask import request
 import pandas as pd
-from io import StringIO
 import json
 
 from core import cache, limiter
 from elastic.elasticsearch import Elastic
 
+from utils import PrintLog, FlaskUtils
+
 api = Api(version='1.0',
           title='Service Discovery API',
           description="** Service Discovery Flask RESTX API **")
 
-insert_ns = api.namespace('service_insert', description='Insert a new service to the registry')
+insert_ns = api.namespace('service_insert', description='Insert a new service to the Elasticsearch registry')
 @insert_ns.route('', methods = ['POST']) # url/user
 class GetStatus(Resource):
     @limiter.limit('1000/hour')
-    @cache.cached(timeout=84600, query_string=True)
+    @cache.cached(timeout=1, query_string=True)
     @api.response(404, 'Data not found')
     @api.response(500, 'Unhandled errors')
     @api.response(400, 'Invalid parameters')
-    @api.doc(params={ 'id': 'Specify the Id associated with the person' })
+    @api.response(503, 'Service Unavailable')
     def post(self):
         if request.method != 'POST':      
             return
 
         # get data from POST
-        content = request.json
-
-        # check if content is empty
-        if content is None:
-            return {'message': 'Empty POST content'}, 400
-
-        # check if content is valid JSON
         try:
-            json.loads(content)
-        except TypeError:
-            return 'Invalid parameter format, only JSON is acepted', 400
+            if not (content := request.get_json()):
+                FlaskUtils.handle400error(insert_ns, "Empty POST data")                
+        except:
+            FlaskUtils.handle400error(insert_ns, "Invalid data format")            
 
         # try to convert the data to a dataframe
         try:
             df = pd.json_normalize(json.loads(content))
         except:
-            return "Invalid data format", 400
+            FlaskUtils.handle400error(insert_ns, "Invalid data format")
+
 
         # remove .0 from the colum names
         df.columns = [col.replace('.0', '') for col in df.columns]
-        
-        # check if df have the right columns and the content of any is not empty
-        if not set(['full_name', 'link', 'stars', 'forks', 'watchers', 'updated_on', 'keywords', 'source', 'uuid']).issubset(df.columns):
-            return "Invalid data format, the inserted columns are not correct to perform an insertion, check that all columns have data.", 400
 
-        # TODO: check if the content of the colunms is not empty
+        # check if df have all the right columns
+        if not {
+            'full_name',
+            'link',
+            'stars',
+            'forks',
+            'watchers',
+            'updated_on',
+            'keywords',
+            'source',
+            'uuid',
+        }.issubset(df.columns):
+            FlaskUtils.handle400error(insert_ns, "Invalid data format, the inserted columns are not correct to perform an insertion, check that all columns.")
 
-        # convert df.source to lowercase
+        # convert df.source content to lowercase str
         df['source'] = df['source'].str.lower()
-  
-        # check the source of the dataframe
-        if df.source.str.contains('github').any() or df.source.str.contains('bitbucket').any() or df.source.str.contains('gitlab').any():
-            return 'Invalid data source, source must not be github or bitbucket or gitlab', 400
-    
-        # insert data to elastic
-        Elastic().upload_pandas(df)
-        # export data to csv usin the file name full_name from the dataframe df
 
+        # as it is a new insertion, it cannot be an source that we are using in the discovery process
+        if df.source.str.contains('github').any() or df.source.str.contains('bitbucket').any() or df.source.str.contains('gitlab').any():
+            FlaskUtils.handle400error(insert_ns, "Invalid data source, source must not be GitHub or Bitbucket or GitLab.")   
+
+        PrintLog.log("[service_insert] Inserting new service to the Elasticsearch registry: \n" + str(df))
+
+        try:               
+            insrt_erros = Elastic().upload_pandas(df)
+            if insrt_erros:
+                FlaskUtils.handle503error(insert_ns,'Insert service is unavailable')
+
+        except Exception as e:
+            PrintLog.log("[service_insert] Insert service is unavailable, check Elasticseartch status")
+
+            FlaskUtils.handle503error(insert_ns, 'Insert service is unavailable')
+        # Export data to csv usin the file name full_name from the dataframe df
         df.to_csv('output/SmartCLIDE_insert_' + df['full_name'][0] + '.csv', index=False)
-       
-        return 'Data inserted', 200
+
+        return "{'status': 'Data inserted correctly', 'statusCode': '200'}"
+        
